a01bfc0454568f08203ad643d17e5f7738a1ea8c
Brandon Craft <bcraft4570@gmail.com>
2014-10-25 00:14:09 -0700

    95% Complete
    Working cleanly but mCM() does not progress through set, get, set, get
    when mCM() is called.
    
    Must call setmat, getmat, settam, gettam to make cacheSolve work.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,73 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function (x = numeric(), y = integer(), z = integer()) {
+        
+        ## Create Local matrix
+        mat <- NULL
+        
+        
+        ## 1st CALL FUNCTION_SET: Create Matrix
+        setmat <- function (a, b, c) {
+                
+                a <- x
+                b <- y
+                c <- z
+                
+                mat <<- matrix(a, b, c)
+        
+                tam <<- NULL
+        }
+        
+        ## 2nd CALL FUNCTION_GET: Return Local Matrix
+        getmat <- function() {
+                
+                return(mat)
+        }
+        
+        ## 3rd CALL FUNCTION_SET: Solve for Inverse of Local Matrix; Assign to Cached Matrix
+        settam <- function(tmp) {
+                
+                tmp <- mat
+                
+                tam <<- solve(tmp)
+        }
+        
+        ## 4th CALL FUNCTION_GET: Return Inverse Matrix
+        gettam <- function() {
+                
+                return(tam)
+        }
+        
+        ## MAIN FUNCTION: Call & Set all Matrix variables Local and Cached
+        list(setmat = setmat, getmat = getmat, settam = settam, gettam = gettam)
+        
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## FUNCTION: Return / Solve for inverse of Cached Matrix
+cacheSolve <- function(x) {
+        
+        ## Search for Cached Inverse Matrix;
+        tam <- x$gettam()
+        
+        ## IF !NOT(IS.NULL(tam) == TRUE);  
+        if(!is.null(tam)) {
+                
+                ## PRINT message while GET cache
+                message("Retrieving cached data.")
+                ## Return Cached Inverse Matrix
+                return(tam)
+        }
+        ## ELSE
+        ## Create Local variable to store Local Matrix using call to  getmat() 
+        data <- x$getmat()
+        
+        ## Create local variable to store Inverse of Matrix
+        tam <- solve(data)
+        
+        ## Call to settam() to assign Inverse Matrix to Cached variable.
+        x$settam(tam)
+        
+        ## Return Cached Inverse Matrix
+        return(tam)
 }
+
+
